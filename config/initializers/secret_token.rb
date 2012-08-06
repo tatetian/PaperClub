812695282eeceaab31e65ab142097d9e18697a67
Hongliang TIAN <tatetian@gmail.com>
2012-08-06 11:37:23 +0800

    RSpec ready to go

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaperClub::Application.config.secret_token = '8d7565ab4937a41c57c7de8eaf50e8828f83617e4bd70fb065f8d52ecca69f5105c1f6df5738cb21f6e0298023121fbd4920e8d0b93ba8bd92dc9a1ed8b9f7cc'
