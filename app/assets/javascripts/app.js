d43fae94a787027789d3edb5fed55f731e9a80a8
Hongliang TIAN <tatetian@gmail.com>
2012-08-25 00:20:54 +0800

    Add routes and add AllClubsScreen to App

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -18,7 +18,7 @@ $(function() {
       clubScreen: {},
       paperScreen: {} 
     },
-
+    currentScreen: null,
     // Backbone routes are used for routing urls with hash tags(#).
     // This enables the user to switch between screens just by changing url.
     // No page refresh is needed.
@@ -55,7 +55,10 @@ $(function() {
     },
 
     switchScreen: function(newScreen) {
-      
+      if(this.currentScreen)
+        this.currentScreen.hide();
+      this.currentScreen = newScreen;
+      newScreen.show();
     },
 
     getCachedScreen: function(name, id) {
@@ -67,7 +70,7 @@ $(function() {
                           userId: ""
                         });
         }
-        return clubScreen;
+        return clubsScreen;
       }
       else if(name=="club") {
         var clubScreen = this.cache.clubScreen[id];
@@ -95,17 +98,73 @@ $(function() {
   //    Screen is the building block of App. One Screen usually offers one 
   //    functionality. Users use the app by switching between different Screen.
   // ==========================================
-  var Screen = PaperClub.screen = Backbone.View.extend({
-    initialize: function(options) {
-    }
-     
+  var Screen = PaperClub.screen = function(options) {
+
+    Backbone.View.apply(this, [options]);
+  }
+
+  _.extend(Screen.prototype, Backbone.View.prototype, {
+    visible: false,
+    show: function() {
+      if(this.visible)
+        return;
+      this.$el.appendTo("body").show();
+      this.visible = true;
+    },
+    hide: function() {
+      if(!this.visible)
+        return;
+      this.$el.detach().hide();      
+      this.visible = false;
+    } 
   });
+    
+  Screen.extend = Backbone.View.extend;
 
   // ==========================================
   //    AllClubsScreen
   // ==========================================
   var AllClubsScreen = PaperClub.AllClubsScreen = Screen.extend({
+    className: "p-page-content shadow024 bgwhite",
     initialize: function(options) {
+      this.render();
+
+      this.initEvents();
+    },
+    template: _.template($("#clubs-screen-template").html()),
+    initEvents: function() {
+      var that = this;
+      $(window).resize(function() { 
+        if(this.visible) that.onResize() 
+      });
+    },
+    render: function() {
+      this.$el.empty()
+              .append(this.template()); 
+      this.onResize();
+      return this;      
+    },
+    onResize: function() {
+      // White area not too short
+			this.$el.css('min-height' , 
+                                    $(window).height() - 24*2);
+      // Size of new club dialog
+      var whtml=$(window).width();
+			var wst_c=(whtml-48-255-18-36)*0.5;
+			this.$('.newclub .m-m-container').css({"width":wst_c+88,"height":310});
+    }
+  });
+  var InvitedClubView = Backbone.View.extend({
+    tagName: "li",
+    className: "font-c mb30 cf",
+    template: _.template($("#invited-club-template").html()),
+    render: function() {
+      // TODO: jioajoidasioddas 
+      this.$el.append(this.template({
+        name: "",
+        description: "",
+     //.. 
+      }));
     }
   });
 
