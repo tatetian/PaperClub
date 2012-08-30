674261cb4ab00cc610152c5b891e0c1ace392b67
Hongliang TIAN <tatetian@gmail.com>
2012-08-31 00:06:11 +0800

    Home page done

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,3 +1,27 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+$(function( ){
+  function onResize() {
+    // Adjust the minimum height of content
+    var $content = $('.p-page-content');
+    $content.css('min-height' , $(window).height() - 48);
+    // Adjust the slides with
+    var imgWidth = 767;//$content.width() - 255 * 2,
+        imgHeight = 413;// * imgWidth / 767, 
+        H = imgHeight + 71 + 48 ;
+    $("#slides").width(imgWidth);
+    $(".slide").height(H).width(imgWidth);
+    $(".slide > img").height(imgHeight);
+    $(".slide > img").width(imgWidth);
+    $(".slide > .caption").width(imgWidth);
+    $(".i-focus-list").css("margin-left", 
+                        ($content.width()-imgWidth)/2);
+  }
+  $(window).resize(onResize);
+  onResize();
+
+  $('#slides').slides({
+    play: 3500,
+    effect: 'fade, fade',
+    pagination: true,
+    preload: true
+  });
+});
